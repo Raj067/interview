012d5d0ed166eab18545f849caab0edbff72aa25
Rajabu Mrisho <rajabumrisho69@gmail.com>
2023-02-15 18:42:54 +0300

    ---

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path("", homepage, name="homepage"),
-    
+    path("add-product/", add_product, name="add_product"),
+    path("edit-product-<int:product_id>/", edit_product, name="edit_product"),
+    path("delete-product-<int:product_id>/",
+         delete_product, name="delete_product"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,49 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Product
 # Create your views here.
 
+
 def homepage(request, *args, **kwargs):
-    return render(request, "home.html", {})
+    data = Product.objects.all()
+    context = {
+        "data": data,
+    }
+    return render(request, "home.html", context)
+
+
+def add_product(request, *args, **kwargs):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+        reg = Product(
+            name=name,
+            price=price,
+            description=description,
+        )
+        reg.save()
+        return redirect('/')
+    return render(request, "add.html", {})
+
+
+def edit_product(request, product_id, *args, **kwargs):
+    data = Product.objects.get(id=product_id)
+    if request.method == "POST":
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+        data.name = name
+        data.price = price
+        data.description = description
+        data.save()
+        return redirect('/')
+    context = {
+        "data": data,
+    }
+    return render(request, "edit.html", context)
+
+
+def delete_product(request, product_id, *args, **kwargs):
+    data = Product.objects.get(id=product_id)
+    data.delete()
+    return redirect('/')
